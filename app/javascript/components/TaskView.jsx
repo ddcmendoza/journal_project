60d2127b0c8890db6cb07845e9a9ff393b80a4b4
Dave <ddcmendoza@gmail.com>
2021-03-27 00:57:37 +0800

    line break display of tasks

--- app/javascript/components/TaskView.jsx
+++ app/javascript/components/TaskView.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function TaskView(props) {
-
+    var parse = require('html-react-parser');
     return (
         <li key={props.task.id} id={props.task.id} onClick={props.taskView}>
             <a onMouseEnter={props.showClose} onMouseLeave={props.hideClose} key={props.task.id} id={props.task.id}>
@@ -22,7 +22,8 @@ export default function TaskView(props) {
                 {props.task.deadline &&
                     <div id={props.task.id} className="badge bg-warning text-dark mx-1">Due in {Math.floor((new Date(props.task.deadline).getTime() - new Date(Date.now()).getTime()) / (1000 * 60 * 60 * 24))} {(Math.floor((new Date(props.task.deadline).getTime() - new Date(Date.now()).getTime()) / (1000 * 60 * 60 * 24)) > 1) ? 'Days' : 'Day'}</div>
                 }
-                <p id={props.task.id}>{props.task.details} </p>
+                <p id={props.task.id} style={{whiteSpace: 'pre-line'}}>{props.task.details} </p>
+
                 <input type="text" className="visually-hidden"></input>
                 <textarea className="visually-hidden"></textarea>
             </a>
