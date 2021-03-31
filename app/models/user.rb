9f1808d38e1e6e73d44105c032d92c7ca64c6950
Dave <ddcmendoza@gmail.com>
2021-03-31 20:48:00 +0800

    test validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
     validates :username, length: {minimum: 4}
     validates :username, length: {maximum: 32}
     validates :password_digest, presence: true
-    validate :password_must_exist
+    #validate :password_must_exist
     has_many  :tasks
     has_many :categories
 
-    def password_must_exist
-        if !(:password_digest)
-            validates :password, length: {minimum: 6}
-            validates :password, length: {maximum: 32}
-        end
-    end
+    # def password_must_exist
+    #     if !(:password_digest)
+    #         validates :password, length: {minimum: 6}
+    #         validates :password, length: {maximum: 32}
+    #     end
+    # end
 end
 
